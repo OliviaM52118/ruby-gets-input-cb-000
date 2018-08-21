c14f8a5a51f706c1f9669a51c3ec83c192404fa1
Olivia Macedo <liom62313@gmail.com>
2018-08-21 00:20:16 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,3 @@
 def greeting(loc_name = "Ruby Programmer")
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+  puts "Hi, #{loc_name}! Welcome to the wonderful world of Ruby programming."
 end
